819b0f72229ef07aacec90fadadf45d2484984d5
Karl Hsu <axray@hotmail.com>
2021-03-29 00:05:15 +0800

    cahange deploy to ssd and small

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React You can get everything!!!          
+          Learn React You can get!!!          
         </a>
       </header>
     </div>
