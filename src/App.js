c29ce2cd943698a2fb6741a596bfa4186640aeb1
Karl Hsu <axray@hotmail.com>
2021-03-28 23:08:03 +0800

    changed app.js a's text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Bye There Cool!!!          
+          Learn React You can get everything!!!          
         </a>
       </header>
     </div>
